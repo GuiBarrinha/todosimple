09df548fedcf94628339efc54ea22590c073aff3
Guilherme Barrinha <guilhermebarrinhams@gmail.com>
2024-08-28 15:36:19 -0300

    criado UserService

--- todosimple/src/main/java/com/guilhermebarrinha/todosimple/repository/TaskRepository.java
+++ todosimple/src/main/java/com/guilhermebarrinha/todosimple/repository/TaskRepository.java
@@ -1,5 +1,7 @@
 package com.guilhermebarrinha.todosimple.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
